1106c2b2dbe5a1542f9102fc55db15c4fdacf8d6
jsakeag <jsakeag@gmail.com>
2022-10-22 16:06:02 -0700

    Modified packages
    attempting to fix vulnerabilities

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { View, Text } from "react-native";
 import Home from "./screens/Home";
+import RestaurantDetail from "./screens/RestaurantDetail";
 import Search from "./screens/Search";
 import Suggest from "./screens/Suggest";
 import Test from "./screens/Test";
 
 export default function App() {
-  return <Home />;
+  return (
+    <Text style={{ textAlign: "center" }}>
+      tap anywhere to create a suggestion room! (invite people here or join by
+      messages)
+    </Text>
+  );
 }
