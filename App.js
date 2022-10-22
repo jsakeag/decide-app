c4aed3c018f5faae2a5dc5954e008e2c603b125b
jsakeag <jsakeag@gmail.com>
2022-10-22 16:10:27 -0700

    Revert "Modified packages"
    This reverts commit 1106c2b2dbe5a1542f9102fc55db15c4fdacf8d6.
    
    Reverted commit

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
 import { View, Text } from "react-native";
 import Home from "./screens/Home";
-import RestaurantDetail from "./screens/RestaurantDetail";
 import Search from "./screens/Search";
 import Suggest from "./screens/Suggest";
 import Test from "./screens/Test";
 
 export default function App() {
-  return (
-    <Text style={{ textAlign: "center" }}>
-      tap anywhere to create a suggestion room! (invite people here or join by
-      messages)
-    </Text>
-  );
+  return <Home />;
 }
